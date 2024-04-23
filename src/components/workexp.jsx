b95a19db7008e5290a884b59865f51ccfd63ca0d
falcon-1024 <abhipatel5058@gmail.com>
2024-04-22 21:23:37 -0400

    work exp read more button removed temararily

--- src/components/workexp.jsx
+++ src/components/workexp.jsx
@@ -108,14 +108,13 @@ const WorkExperience = () => {
               <p className="text-sm md:text-lg min-[2300px]:text-3xl text-neutral-500 dark:text-neutral-50">
                 - Jun 2019 - Jul 2019
               </p>
-
-              {/* <!-- Button --> */}
-              <a
+              {/* TODO : add modal to show the duties */}
+              {/* <a
                 href="#"
                 className="bottom-0 mt-3 inline-block rounded bg-neutral-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-blue-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
               >
                 Read More
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
